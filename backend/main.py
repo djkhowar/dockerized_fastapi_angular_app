d7aebb1d1e2b3cf4979efd05cfa6bec85c020d6a
djkhowar <djkhowar@gmail.com>
2024-07-15 00:08:15 +0530

    added custom env file

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from uuid import uuid4
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 import os
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Access the variables
 username = os.getenv('username')
